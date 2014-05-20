29733c1379e56e1338dc5825eb3564c37d9521b3
Thomas Anten <thomas@tanten.nl>
2014-05-20 15:00:36 +0200

    Het eea aangepast!

--- CM/index.php
+++ CM/index.php
@@ -14,7 +14,7 @@
 <body>
 
 <section id="cue">
-<p>Inkomende chats</p>
+<h1>Inkomende chats</h1>
 	<?php
     $result = mysqli_query($db,"SELECT * FROM chats");
 	$lastId = mysqli_query($db,"SELECT MAX(id) FROM chats");
@@ -32,84 +32,25 @@
     ?> 
 </section>
 <section id="private">
-<p>Prive chats</p>
+<h1>Geaccepteerde chats</h1>
 </section>
 
 <section id="chat">
 
 </section>
+<script src="http://html5demos.com/h5utils.js"></script>
 <script>
-	if (Modernizr.draganddrop) {
-		$( "#private" ).text('Sleep een chat om te accepteren');
 
-	var dragSrcEl = null;
-	
-	function handleDragStart(e) {
-	  // Target (this) element is the source node.
-	  this.style.opacity = '0.4';
-	
-	  dragSrcEl = this;
-	
-	  e.dataTransfer.effectAllowed = 'move';
-	  e.dataTransfer.setData('text/html', this.innerHTML);
-	}
-	
-	function handleDragOver(e) {
-	  if (e.preventDefault) {
-		e.preventDefault(); // Necessary. Allows us to drop.
-	  }
-	
-	  e.dataTransfer.dropEffect = 'move';  // See the section on the DataTransfer object.
-	
-	  return false;
-	}
-	
-	function handleDragEnter(e) {
-	  // this / e.target is the current hover target.
-	  this.classList.add('over');
-	}
-	
-	function handleDragLeave(e) {
-	  this.classList.remove('over');  // this / e.target is previous target element.
-	}
-	
-	function handleDrop(e) {
-	  // this/e.target is current target element.
-	
-	  if (e.stopPropagation) {
-		e.stopPropagation(); // Stops some browsers from redirecting.
-	  }
-	
-	  // Don't do anything if dropping the same column we're dragging.
-	  if (dragSrcEl != this) {
-		// Set the source column's HTML to the HTML of the column we dropped on.
-		dragSrcEl.innerHTML = this.innerHTML;
-		this.innerHTML = e.dataTransfer.getData('text/html');
-	  }
-	
-	  return false;
-	}
-	
-	function handleDragEnd(e) {
-	  // this/e.target is the source node.
-	
-	  [].forEach.call(cols, function (col) {
-		col.classList.remove('over');
-	  });
-	}
-	
-	var cols = document.querySelectorAll('#cue .chat');
-	[].forEach.call(cols, function(col) {
-	  col.addEventListener('dragstart', handleDragStart, false);
-	  col.addEventListener('dragenter', handleDragEnter, false)
-	  col.addEventListener('dragover', handleDragOver, false);
-	  col.addEventListener('dragleave', handleDragLeave, false);
-	  col.addEventListener('drop', handleDrop, false);
-	  col.addEventListener('dragend', handleDragEnd, false);
+	$(this).click(function() {
+		$(this).appendTo("#private");
 	});
+	
+	if (Modernizr.draganddrop) {
+		//$( "#private h1" ).text('Sleep een chat om te accepteren');
+	
 		
 	} else {
-		$( "#private" ).text('Klik om een chat te accepteren')
+		//$( "#private h1" ).text('Klik om een chat te accepteren')
 	}
 
 	setInterval(	
@@ -126,7 +67,7 @@
 		var vname = data[1];			//get name
 		var vmsg = data[2];			//get msg
 		var vdate = data[3];			//get date
-		$('#cue').append("<section class='chat' draggable='true'><header><h2>"+vname+"</h2><small>"+vdate+"</small></header><p>"+vmsg+"</p></section>");
+		$('#cue').append("<section class='chat' id='"+id+"' draggable='true'><header><h2>"+vname+"</h2><small>"+vdate+"</small></header><p>"+vmsg+"</p></section>");
 	  }
 	});
 	}, 2000);
